3cbafbea2d6cbf7624bd18b217a24dd8f84d2182
rompt-promt <revstropov@gmail.com>
2022-11-01 14:30:42 +0300

    added vertical slider

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const container1 = document.querySelectorAll('.js-res')[0];
+const container2 = document.querySelectorAll('.js-res')[1];
 
 const slider1 = new SliderController({
     root: container1, 
     isVertical: false,
+    useRange: 0,
+})
+
+const slider2 = new SliderController({
+    root: container2, 
+    isVertical: 1,
     useRange: 0,
 })
