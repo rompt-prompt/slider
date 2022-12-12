c0273c15f2e6829a02d4fcaa8d1d4007131ddbdc
Roman <revstropov@gmail.com>
2022-12-12 12:32:12 +0300

    fix config for dates

--- configurator/configurator.js
+++ configurator/configurator.js
@@ -8,7 +8,7 @@ class Configurator {
         this.createConfigInstances();
         this.createForm();
         this.form.onchange = (event) => {
-            this.onChange(event.target.name);
+            this.onChange(event.target);
         };
     }
 
@@ -26,13 +26,22 @@ class Configurator {
         this.config.push(new RangeOption(this.slider))
     }
 
-    onChange(name) {
+    onChange(target) {
+        let optionsBackUp = Object.assign({}, this.slider.options)
+        let name = target.name
         this.config.find(option => {
             if(option.optionName === name) {
                 option.update()
+                if(this.slider.validator.isValidOptions(this.slider.options)) {
+                    this.slider.reset()
+                } else {
+                    this.slider.options = Object.assign(this.slider.options, optionsBackUp);
+                    option.setValueAttr();
+                    console.log(this.slider.validator.errors); // TODO in div output
+                }
             }
         })
-        this.slider.reset()
+        
 
     }
 
@@ -60,29 +69,51 @@ class RangeOption extends ConfigurableOption {
         this.min = new Input(this.elem, 'Минимум', {
             name: this.optionName,
             type: slider.options.dataType,
-            value: slider.options.range[0],
+            // value: slider.options.range[0],
             max: limits.min
         });
         this.max = new Input(this.elem, 'Максимум', {
             name: this.optionName,
             type: slider.options.dataType,
-            value: slider.options.range[1],
+            // value: slider.options.range[1],
             min: limits.max
         });
+        this.setValueAttr();
+    }
+
+    setValueAttr() {
+        switch(this.slider.options.dataType) {
+            case 'date':
+                this.min.value = this.slider.typeHandler.formatDate(this.slider.options.range[0])
+                this.max.value = this.slider.typeHandler.formatDate(this.slider.options.range[1])
+                break;
+            case 'number':
+                this.min.value = this.slider.options.range[0];
+                this.max.value = this.slider.options.range[1];
+                break;
+        }
     }
 
     update() {
-        console.log(+this.min.value, +this.max.value)
+        console.log(this.min.value, this.max.value)
         this.slider.options.range = [+this.min.value, +this.max.value];
+        switch(this.slider.options.dataType) {
+            case 'date':
+                this.slider.options.range = [new Date(this.min.value), new Date(this.max.value)];
+                break;
+            case 'number':
+                this.slider.options.range = [+this.min.value, +this.max.value];
+                break;
+        }
     }
 
     calcLimits(slider) {
         const minCore = slider.model.getClosestId(0);
         const maxCore = slider.model.getClosestId(100);
 
-        const min = slider.model.cores[minCore].verbalValue;
-        const max = slider.model.cores[maxCore].verbalValue;
-        
+        let min = slider.model.cores[minCore].verbalValue;
+        let max = slider.model.cores[maxCore].verbalValue;
+
         return {min, max}
     }
 }
